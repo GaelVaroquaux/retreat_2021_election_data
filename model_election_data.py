510c5ed784f51b6d6abacd8c0e3d8d663916cb78
Gael Varoquaux <gael.varoquaux@normalesup.org>
2021-09-22 12:39:04 +0200

    Feauture importances

--- model_election_data.py
+++ model_election_data.py
@@ -2,6 +2,8 @@
 Our statistical modeling
 """
 
+#######################################################################################
+# Load and wrangle the data
 import pandas as pd
 
 df = pd.read_csv('results/concatenate_all_election.csv')
@@ -15,8 +17,12 @@ outcomes = df_wide['legislatives_2012']
 print("Number of times a party did not present a candidate as the legislatives_2012")
 print(outcomes.isna().sum())
 
+outcomes = outcomes.fillna(value=0)
+
 data = df_wide.drop('legislatives_2012', axis=1)
 
+#######################################################################################
+# Fit a model predicting the major parties
 from sklearn.experimental import enable_hist_gradient_boosting  # noqa
 from sklearn import model_selection, ensemble
 
@@ -30,7 +36,6 @@ prediction_scores = dict()
 # EXG = "Extreme Gauche"
 for target_nuance in ['FN', 'SOC', 'UMP', 'FG', 'EXG']:
     y = outcomes[target_nuance]
-    y = y.fillna(value=0)
 
     prediction_scores[target_nuance] = model_selection.cross_val_score(model, data, y, cv=cv)
 
@@ -47,3 +52,40 @@ plt.xlabel('Prediction score (r2)')
 plt.savefig('results/prediction_scores_per_party.png',
             bbox_inches='tight', dpi=200)
 
+
+#######################################################################################
+# Feature importance for FN
+from sklearn.inspection import permutation_importance
+y = outcomes['FN']
+
+X_train, X_val, y_train, y_val = model_selection.train_test_split(data, y)
+model.fit(X_train, y_train)
+r = permutation_importance(model, X_val, y_val,
+                           n_repeats=30,
+                           random_state=0)
+
+importances = pd.DataFrame(r['importances'].T)
+importances.columns = data.columns
+
+avg_importance_per_party = importances.mean().unstack().fillna(value=0).mean()
+
+# To lighten up the plot, don't display parties for which the average
+# importance is below the median
+select_party = avg_importance_per_party > avg_importance_per_party.median()
+select_party = select_party[select_party].index
+
+importances = importances.unstack().rename('importance').reset_index().drop('level_2', axis=1)
+
+importances = importances.query('Nuance in @select_party')
+
+# Work on the name of the election to have better ordering
+elections = importances['election'].str.split('_')
+importances['election'] = elections.str[1].str.cat(elections.str[0], sep=' ')
+importances = importances.sort_values('election')
+
+plt.figure(figsize=(6, 12))
+seaborn.boxplot(data=importances, x='importance', y='Nuance', hue='election', orient='h',
+                fliersize=1)
+plt.title('Feature import (FN prediction)')
+plt.savefig('results/feature_import_fn.png',
+            bbox_inches='tight', dpi=200)
